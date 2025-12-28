40bc14a50fc757d8958b884dac3fdf5010087e6a
steven <mrwritersteven@gmail.com>
2025-12-28 15:57:26 +0530

    Initial commit: add main.py and .gitignore

---
+++ main.py
@@ -0,0 +1,18 @@
+import  pandas  as pd
+
+df1 = pd.read_csv("./daily_sales_data_0.csv")
+df2 = pd.read_csv("./daily_sales_data_1.csv")
+df3 = pd.read_csv("./daily_sales_data_2.csv")
+
+
+
+df1 = df1[df1["product"]=="pink morsel"]
+df2 = df2[df2["product"]=="pink morsel"]
+df3 = df3[df3["product"]=="pink morsel"]
+
+
+
+final_df = pd.concat([df1,df2,df3],ignore_index=True)
+
+
+final_df["sales"] = final_df["price"]*final_df["quantity"]
